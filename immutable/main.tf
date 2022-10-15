2ec5d60dae76a56829641802ea2ccdca28616a26
Raghu K <raghuk.vit@gmail.com>
2022-10-15 08:31:49 +0530

    Update infra/cart-infra/

--- immutable/main.tf
+++ immutable/main.tf
@@ -2,7 +2,6 @@ module "cart" {
   source           = "./vendor/modules/app"
   COMPONENT        = var.COMPONENT
   ENV              = var.ENV
-  INSTANCES        = var.INSTANCES
   APP_PORT         = 8080
   LB_RULE_PRIORITY = 1
   APP_VERSION      = var.APP_VERSION
